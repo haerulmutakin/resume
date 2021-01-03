df221e0f3e455655232df350b58b4e70d499b4c1
haerulmutakin <haerul.mutakin@uii.ac.id>
2021-01-03 16:20:48 +0700

    add app routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import './App.scss';
-import Header from './components/common/header/Header';
 import AppRoutes from './router/Routes';
+import Header from './components/common/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AppRoutes />
+      <div className="content">
+        <AppRoutes />
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-/* project structure
---components
------ common
------ pages
------ shared
---helpers
------ browser-history
---redux
------ actions
------ reducers
---router
------ index
-*/
--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import BrowserHistory from '../helpers/history/BrowserHistory';
+
+// components
 import Home from '../components/pages/home/Home';
+import Projects from '../components/pages/projects/Projects';
+import Resume from '../components/pages/resume/Resume';
 
 const AppRoutes = () => {
     return (
         <Router history={BrowserHistory}>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/projects" component={Projects} />
+                <Route exact path="/resume" component={Resume} />
             </Switch>
         </Router>
     )
