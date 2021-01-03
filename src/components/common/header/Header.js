b0133944e87e59abeffacdbbec7de6d314e6dc8f
haerulmutakin <haerul.mutakin@uii.ac.id>
2021-01-03 16:20:11 +0700

    update header style

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import './Header.scss';
+import BrowserHistory from '../../../helpers/history/BrowserHistory';
+
+const menus = [
+    {
+        name: 'ABOUT ME',
+        url: '/'
+    },
+    {
+        name: 'RESUME',
+        url: 'resume'
+    },
+    {
+        name: 'PROJECTS',
+        url: 'projects'
+    },
+    {
+        name: 'CONTACT',
+        url: 'contact'
+    },
+];
+
+const handleMenuClick = (url) => {
+    BrowserHistory.push(url);
+}
 
 const Header = () => {
     return (
         <div className="header">
-            <div>
+            <div className="site-brand">
                 <h1>Haerul Mutakin</h1>
                 <p>Software Engineer/Frontend</p>
             </div>
             <ul className="menu-list">
-                <li>ABOUT ME</li>
-                <li>RESUME</li>
-                <li>PROJECTS</li>
-                <li>CONTACT</li>
+                {menus.map((item, index) => {
+                    return <li key={index} onClick={handleMenuClick.bind(this, item.url)}>{item.name}</li>
+                })}
             </ul>
         </div>
     )
