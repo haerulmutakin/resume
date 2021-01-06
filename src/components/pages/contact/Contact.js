064b2ec5ac307a6b0b95071f28eb6813cae6a2ac
haerulmutakin <haerul.mutakin@uii.ac.id>
2021-01-06 21:55:50 +0700

    add contact component

---
+++ src/components/pages/contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Contact.scss';
+
+const Contact = () => {
+    return (
+        <React.Fragment>
+            <div className="content-header">
+                <h2 className="page-title">Contact</h2>
+            </div>
+        </React.Fragment>
+    );
+};
+
+export default Contact;
